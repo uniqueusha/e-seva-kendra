93d8441e259e7f4b4c63b6095092a0396e29e102
poorvamohite2001 <poorvamohite2001@gmail.com>
2024-09-05 09:58:31 +0530

    add change password and email id exist API

--- src/routes/admin/user.route.js
+++ src/routes/admin/user.route.js
@@ -4,6 +4,7 @@ const userController = require("../../controllers/admin/user.controller");
 const checkAuth = require("../../middleware/check.auth");
 
 router.post('/',userController.addUser);
+router.post('/check-emailid',userController.checkEmailId);
 router.get('/',userController.getUsers);
 router.post('/login',userController.userLogin);
 router.get('/status',userController.getStatusWma);
@@ -11,6 +12,7 @@ router.get('/wma',userController.getUserWma);
 router.get('/employee',userController.getUserEmployee);
 router.get('/operator',userController.getOperatorList);
 router.get('/:id',userController.getUser);
+router.put('/change-password',userController.onPasswordChange);
 router.put('/:id',userController.updateUser);
 router.patch('/:id',userController.onStatusChange);
 
