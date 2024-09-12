c2d4d67575c41f751121e0c7a5558732df1322b2
uniqueusha <ushamyadav777@gmail.com>
2024-09-12 15:13:11 +0530

    active status array dashboard

--- src/controllers/admin/adha_dashboard.controller.js
+++ src/controllers/admin/adha_dashboard.controller.js
@@ -34,20 +34,20 @@ const getVerificationStatusCount = async (req, res) => {
     try {
         // Start a transaction
         await connection.beginTransaction();
-        
+
         let adha_verification_status_total_list_count = 0;
         let adha_verification_status_total_amount_count = 0;
         let verification_status_counts = [];
-        
+
         // today total status count
         let todayTotalVerificationStatusCountQuery = `SELECT COUNT(*) AS total FROM verification_status vs
             JOIN adha a
             ON vs.verification_status_id = a.verification_status_id
             WHERE Date(a.created_at) = ? AND vs.status = 1`;
-            if (user_id) {
-                todayTotalVerificationStatusCountQuery += ` AND a.user_id = '${user_id}'`;
-            } 
-        let todayTotalVerificationStatusCountResult = await connection.query(todayTotalVerificationStatusCountQuery,[created_at]);
+        if (user_id) {
+            todayTotalVerificationStatusCountQuery += ` AND a.user_id = '${user_id}'`;
+        }
+        let todayTotalVerificationStatusCountResult = await connection.query(todayTotalVerificationStatusCountQuery, [created_at]);
         adha_verification_status_total_list_count = parseInt(todayTotalVerificationStatusCountResult[0][0].total);
 
         // amount total
@@ -55,11 +55,11 @@ const getVerificationStatusCount = async (req, res) => {
             JOIN adha a
             ON vs.verification_status_id = a.verification_status_id
             WHERE Date(a.created_at) = ? AND vs.status = 1`;
-            if (user_id) {
-                adhaVerificationStatusTotalAmountCountQuery += ` AND a.user_id = '${user_id}'`;
-            }
-        let adhaVerificationStatusTotalAmountCountResult = await connection.query(adhaVerificationStatusTotalAmountCountQuery,[created_at]);
-        adha_verification_status_total_amount_count = parseInt( adhaVerificationStatusTotalAmountCountResult[0][0].total);
+        if (user_id) {
+            adhaVerificationStatusTotalAmountCountQuery += ` AND a.user_id = '${user_id}'`;
+        }
+        let adhaVerificationStatusTotalAmountCountResult = await connection.query(adhaVerificationStatusTotalAmountCountQuery, [created_at]);
+        adha_verification_status_total_amount_count = parseInt(adhaVerificationStatusTotalAmountCountResult[0][0].total);
 
         // specific today total status count
         let specificVerificationStatusCountQuery = `
@@ -68,38 +68,40 @@ const getVerificationStatusCount = async (req, res) => {
             JOIN adha a
             ON vs.verification_status_id = a.verification_status_id
             WHERE Date(a.created_at) = ? AND vs.status = 1`;
-            if (user_id) {
-                specificVerificationStatusCountQuery += ` AND a.user_id = '${user_id}'`;
-            }
-            specificVerificationStatusCountQuery += ` GROUP BY vs.verification_status_id, vs.verification_status`; 
-        let specificVerificationStatusCountResult= await connection.query(specificVerificationStatusCountQuery,[created_at]);
-        
+        if (user_id) {
+            specificVerificationStatusCountQuery += ` AND a.user_id = '${user_id}'`;
+        }
+        specificVerificationStatusCountQuery += ` GROUP BY vs.verification_status_id, vs.verification_status`;
+        let specificVerificationStatusCountResult = await connection.query(specificVerificationStatusCountQuery, [created_at]);
+
         const statusCount = {};
         const amountTotal = {};
         specificVerificationStatusCountResult[0].forEach(row => {
             statusCount[row.verification_status_id] = parseInt(row.total);
             amountTotal[row.verification_status_id] = parseInt(row.total_amount);
         });
-        
+
         //get all verification status
-        let allVerificationStatusesQuery = `SELECT verification_status_id, verification_status FROM verification_status `;
+        let allVerificationStatusesQuery = `SELECT verification_status_id, verification_status, status FROM verification_status `;
         let allVerificationStatusesResult = await connection.query(allVerificationStatusesQuery);
 
         allVerificationStatusesResult[0].forEach(rows => {
-            verification_status_counts.push({
-                verification_status_id: rows.verification_status_id,
-                verification_status: rows.verification_status,
-                verification_status_total: statusCount[rows.verification_status_id] || 0,
-                amount_total :amountTotal[rows.verification_status_id] || 0
-            });
+            if (rows.status === 1) {
+                verification_status_counts.push({
+                    verification_status_id: rows.verification_status_id,
+                    verification_status: rows.verification_status,
+                    verification_status_total: statusCount[rows.verification_status_id] || 0,
+                    amount_total: amountTotal[rows.verification_status_id] || 0
+                });
+            }
         });
-       
+
         const data = {
             status: 200,
             message: "adha dashboard Status Count retrieved successfully",
-            adha_verification_status_total_list_count:adha_verification_status_total_list_count,
-            adha_verification_status_total_amount_count:adha_verification_status_total_amount_count,
-            verification_status_counts:verification_status_counts
+            adha_verification_status_total_list_count: adha_verification_status_total_list_count,
+            adha_verification_status_total_amount_count: adha_verification_status_total_amount_count,
+            verification_status_counts: verification_status_counts
         };
 
         return res.status(200).json(data);
@@ -119,32 +121,32 @@ const getPaymentStatusCount = async (req, res) => {
     try {
         // Start a transaction
         await connection.beginTransaction();
-        
+
         let adha_payment_status_total_list_count = 0;
         let adha_payment_status_total_amount_count = 0;
         let payment_status_counts = [];
-        
+
         // today total status count
         let todayTotalPaymentStatusCountQuery = `SELECT COUNT(*) AS total FROM payment_status ps
             JOIN adha a
             ON ps.payment_status_id = a.payment_status_id
             WHERE Date(a.created_at) = ? AND ps.status = 1`;
-            if (user_id) {
-                todayTotalPaymentStatusCountQuery += ` AND a.user_id = '${user_id}'`;
-            }
-        let todayTotalPaymentStatusCountResult = await connection.query(todayTotalPaymentStatusCountQuery,[created_at]);
+        if (user_id) {
+            todayTotalPaymentStatusCountQuery += ` AND a.user_id = '${user_id}'`;
+        }
+        let todayTotalPaymentStatusCountResult = await connection.query(todayTotalPaymentStatusCountQuery, [created_at]);
         adha_payment_status_total_list_count = parseInt(todayTotalPaymentStatusCountResult[0][0].total);
-        
+
         // amount total
         let adhaPaymentStatusTotalAmountCountQuery = `SELECT SUM(a.amount) AS total FROM payment_status ps
             JOIN adha a
             ON ps.payment_status_id = a.payment_status_id
             WHERE Date(a.created_at) = ? AND ps.status = 1`;
-            if (user_id) {
-                adhaPaymentStatusTotalAmountCountQuery += ` AND a.user_id = '${user_id}'`;
-            }
-            let adhaPaymentStatusTotalAmountCountResult = await connection.query(adhaPaymentStatusTotalAmountCountQuery,[created_at]);
-            adha_payment_status_total_amount_count = parseInt(adhaPaymentStatusTotalAmountCountResult[0][0].total);
+        if (user_id) {
+            adhaPaymentStatusTotalAmountCountQuery += ` AND a.user_id = '${user_id}'`;
+        }
+        let adhaPaymentStatusTotalAmountCountResult = await connection.query(adhaPaymentStatusTotalAmountCountQuery, [created_at]);
+        adha_payment_status_total_amount_count = parseInt(adhaPaymentStatusTotalAmountCountResult[0][0].total);
 
         // specific today total status count
         let specificPaymentStatusCountQuery = `
@@ -153,12 +155,12 @@ const getPaymentStatusCount = async (req, res) => {
             JOIN adha a
             ON ps.payment_status_id = a.payment_status_id
             WHERE Date(a.created_at) = ? AND ps.status = 1`;
-            if (user_id) {
-                specificPaymentStatusCountQuery += ` AND a.user_id = '${user_id}'`;
-            }
-            specificPaymentStatusCountQuery += ` GROUP BY ps.payment_status_id, ps.payment_status`;
-        let specificPaymentStatusCountResult = await connection.query(specificPaymentStatusCountQuery,[created_at]);
-    
+        if (user_id) {
+            specificPaymentStatusCountQuery += ` AND a.user_id = '${user_id}'`;
+        }
+        specificPaymentStatusCountQuery += ` GROUP BY ps.payment_status_id, ps.payment_status`;
+        let specificPaymentStatusCountResult = await connection.query(specificPaymentStatusCountQuery, [created_at]);
+
         const statusCount = {};
         const amountTotal = {};
         specificPaymentStatusCountResult[0].forEach(row => {
@@ -167,24 +169,26 @@ const getPaymentStatusCount = async (req, res) => {
         });
 
         //get all payment status
-        let allPaymentStatusesQuery = `SELECT payment_status_id, payment_status FROM payment_status`;
+        let allPaymentStatusesQuery = `SELECT payment_status_id, payment_status, status FROM payment_status`;
         let allPaymentStatusesResult = await connection.query(allPaymentStatusesQuery);
 
         allPaymentStatusesResult[0].forEach(rows => {
-            payment_status_counts.push({
-                payment_status_id: rows.payment_status_id,
-                payment_status: rows.payment_status,
-                payment_status_total: statusCount[rows.payment_status_id] || 0,
-                amount_total: amountTotal[rows.payment_status_id] || 0
-            });
+            if (rows.status === 1) {
+                payment_status_counts.push({
+                    payment_status_id: rows.payment_status_id,
+                    payment_status: rows.payment_status,
+                    payment_status_total: statusCount[rows.payment_status_id] || 0,
+                    amount_total: amountTotal[rows.payment_status_id] || 0
+                });
+            }
         });
-        
+
         const data = {
             status: 200,
             message: "adha dashboard Status Count retrieved successfully",
-            adha_payment_status_total_list_count:adha_payment_status_total_list_count,
-            adha_payment_status_total_amount_count:adha_payment_status_total_amount_count,
-            payment_status_counts:payment_status_counts
+            adha_payment_status_total_list_count: adha_payment_status_total_list_count,
+            adha_payment_status_total_amount_count: adha_payment_status_total_amount_count,
+            payment_status_counts: payment_status_counts
         };
 
         return res.status(200).json(data);
--- src/controllers/admin/task_dashboard.controller.js
+++ src/controllers/admin/task_dashboard.controller.js
@@ -25,7 +25,7 @@ error500 = (error, res) => {
         error: error
     });
 };
- 
+
 //getStatusCount
 const getStatusCount = async (req, res) => {
     const { created_at, user_id, assigned_to } = req.query;
@@ -35,39 +35,39 @@ const getStatusCount = async (req, res) => {
     try {
         // Start a transaction
         await connection.beginTransaction();
-        
+
         let task_status_total_list_count = 0;
         let task_status_total_amount_count = 0;
         let status_counts = [];
-        
+
         // today total status count
         let todayTotalStatusCountQuery = `SELECT COUNT(*) AS total FROM status s
             JOIN task_header th
             ON s.status_id = th.status_id
             WHERE Date(th.created_at) = ? AND s.status = 1`;
-            if (user_id) {
-                todayTotalStatusCountQuery += ` AND th.user_id = '${user_id}'`;
-            } 
-            if (assigned_to) {
-                todayTotalStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
-            }
-        let todayTotalStatusCountResult = await connection.query(todayTotalStatusCountQuery,[created_at]);
+        if (user_id) {
+            todayTotalStatusCountQuery += ` AND th.user_id = '${user_id}'`;
+        }
+        if (assigned_to) {
+            todayTotalStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
+        }
+        let todayTotalStatusCountResult = await connection.query(todayTotalStatusCountQuery, [created_at]);
         task_status_total_list_count = parseInt(todayTotalStatusCountResult[0][0].total);
-        
+
         //amount total
         let taskStatusTotalAmountCountQuery = `SELECT SUM(th.amount) AS total FROM status s
             JOIN task_header th
             ON s.status_id = th.status_id
             WHERE Date(th.created_at) = ? AND s.status = 1`;
-            if (user_id) {
-                taskStatusTotalAmountCountQuery += ` AND th.user_id = '${user_id}'`;
-            } 
-            if (assigned_to) {
-                taskStatusTotalAmountCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
-            }
-        let taskStatusTotalAmountCountResult = await connection.query(taskStatusTotalAmountCountQuery,[created_at]);
+        if (user_id) {
+            taskStatusTotalAmountCountQuery += ` AND th.user_id = '${user_id}'`;
+        }
+        if (assigned_to) {
+            taskStatusTotalAmountCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
+        }
+        let taskStatusTotalAmountCountResult = await connection.query(taskStatusTotalAmountCountQuery, [created_at]);
         task_status_total_amount_count = parseInt(taskStatusTotalAmountCountResult[0][0].total);
-        
+
         // specific today total status count
         let specificStatusCountQuery = `
             SELECT th.status_id,s.status_name, SUM(th.amount) AS total_amount, COUNT(*) AS total
@@ -75,16 +75,16 @@ const getStatusCount = async (req, res) => {
             JOIN task_header th
             ON s.status_id = th.status_id
             WHERE Date(th.created_at) = ? AND s.status = 1`;
-            if (user_id) {
-                specificStatusCountQuery += ` AND th.user_id = '${user_id}'`;
-            }
-            if (assigned_to) {
-                specificStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
-            }
+        if (user_id) {
+            specificStatusCountQuery += ` AND th.user_id = '${user_id}'`;
+        }
+        if (assigned_to) {
+            specificStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
+        }
 
-            specificStatusCountQuery += ` GROUP BY s.status_id, s.status_name `;
-        let specificStatusCountResult = await connection.query(specificStatusCountQuery,[created_at]);
-        
+        specificStatusCountQuery += ` GROUP BY s.status_id, s.status_name `;
+        let specificStatusCountResult = await connection.query(specificStatusCountQuery, [created_at]);
+
         const statusCount = {};
         const amountTotal = {};
         specificStatusCountResult[0].forEach(row => {
@@ -94,24 +94,26 @@ const getStatusCount = async (req, res) => {
         });
 
         //get all status
-        let allStatusesQuery = `SELECT status_id, status_name FROM status`;
+        let allStatusesQuery = `SELECT status_id, status_name, status FROM status`;
         let allStatusesResult = await connection.query(allStatusesQuery);
 
         allStatusesResult[0].forEach(rows => {
-            status_counts.push({
-                status_id: rows.status_id,
-                status_name: rows.status_name,
-                status_total: statusCount[rows.status_id] || 0,
-                amount_total: amountTotal[rows.status_id] || 0
-            })
+            if (rows.status === 1) {
+                status_counts.push({
+                    status_id: rows.status_id,
+                    status_name: rows.status_name,
+                    status_total: statusCount[rows.status_id] || 0,
+                    amount_total: amountTotal[rows.status_id] || 0
+                });
+            }
         });
-        
+
         const data = {
             status: 200,
             message: "Task dashboard Status Count retrieved successfully",
-            task_status_total_list_count:task_status_total_list_count,
-            task_status_total_amount_count:task_status_total_amount_count,
-            status_counts:status_counts
+            task_status_total_list_count: task_status_total_list_count,
+            task_status_total_amount_count: task_status_total_amount_count,
+            status_counts: status_counts
         };
 
         return res.status(200).json(data);
@@ -131,39 +133,39 @@ const getPaymentStatusCount = async (req, res) => {
     try {
         // Start a transaction
         await connection.beginTransaction();
-        
+
         let task_payment_status_total_list_count = 0;
         let adha_payment_status_total_amount_count = 0;
         let payment_status_counts = [];
-        
+
         // today total status count
         let todayTotalPaymentStatusCountQuery = `SELECT COUNT(*) AS total FROM payment_status ps 
             JOIN task_header th
             ON ps.payment_status_id = th.payment_status_id
             WHERE Date(th.created_at) = ? AND ps.status = 1`;
-            if (user_id) {
-                todayTotalPaymentStatusCountQuery += ` AND th.user_id = '${user_id}'`;
-            } 
-            if (assigned_to) {
-                todayTotalPaymentStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
-            }
-        let todayTotalPaymentStatusCountResult = await connection.query(todayTotalPaymentStatusCountQuery,[created_at]);
+        if (user_id) {
+            todayTotalPaymentStatusCountQuery += ` AND th.user_id = '${user_id}'`;
+        }
+        if (assigned_to) {
+            todayTotalPaymentStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
+        }
+        let todayTotalPaymentStatusCountResult = await connection.query(todayTotalPaymentStatusCountQuery, [created_at]);
         task_payment_status_total_list_count = parseInt(todayTotalPaymentStatusCountResult[0][0].total);
-        
+
         //amount total
-        let adhaPaymentStatusTotalAmountCountQuery =`SELECT SUM(th.amount) AS total FROM payment_status ps 
+        let adhaPaymentStatusTotalAmountCountQuery = `SELECT SUM(th.amount) AS total FROM payment_status ps 
             JOIN task_header th
             ON ps.payment_status_id = th.payment_status_id
             WHERE Date(th.created_at) = ? AND ps.status = 1`;
-            if (user_id) {
-                adhaPaymentStatusTotalAmountCountQuery += ` AND th.user_id = '${user_id}'`;
-            } 
-            if (assigned_to) {
-                adhaPaymentStatusTotalAmountCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
-            }
-        let adhaPaymentStatusTotalAmountCountResult = await connection.query(adhaPaymentStatusTotalAmountCountQuery,[created_at]);
+        if (user_id) {
+            adhaPaymentStatusTotalAmountCountQuery += ` AND th.user_id = '${user_id}'`;
+        }
+        if (assigned_to) {
+            adhaPaymentStatusTotalAmountCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
+        }
+        let adhaPaymentStatusTotalAmountCountResult = await connection.query(adhaPaymentStatusTotalAmountCountQuery, [created_at]);
         adha_payment_status_total_amount_count = parseInt(adhaPaymentStatusTotalAmountCountResult[0][0].total);
-        
+
         // specific today total payment status count
         let specificStatusCountQuery = `
             SELECT th.payment_status_id,ps.payment_status, SUM(th.amount) AS total_amount,COUNT(*) AS total
@@ -171,14 +173,14 @@ const getPaymentStatusCount = async (req, res) => {
             JOIN task_header th
             ON ps.payment_status_id = th.payment_status_id
             WHERE Date(th.created_at) = ? AND ps.status = 1`;
-            if (user_id) {
-                specificStatusCountQuery += ` AND th.user_id = '${user_id}'`;
-            } 
-            if (assigned_to) {
-                specificStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
-            }
-            specificStatusCountQuery += ` GROUP BY ps.payment_status_id, ps.payment_status `;
-        let specificStatusCountResult = await connection.query(specificStatusCountQuery,[created_at]);
+        if (user_id) {
+            specificStatusCountQuery += ` AND th.user_id = '${user_id}'`;
+        }
+        if (assigned_to) {
+            specificStatusCountQuery += ` AND th.assigned_to = '${assigned_to}'`;
+        }
+        specificStatusCountQuery += ` GROUP BY ps.payment_status_id, ps.payment_status `;
+        let specificStatusCountResult = await connection.query(specificStatusCountQuery, [created_at]);
 
         const statusCount = {};
         const amountTotal = {};
@@ -189,24 +191,26 @@ const getPaymentStatusCount = async (req, res) => {
         });
 
         //get all payment status
-        let allPaymentStatusesQuery = `SELECT payment_status_id, payment_status FROM payment_status`;
+        let allPaymentStatusesQuery = `SELECT payment_status_id, payment_status, status FROM payment_status`;
         let allPaymentStatusesResult = await connection.query(allPaymentStatusesQuery);
 
         allPaymentStatusesResult[0].forEach(rows => {
-            payment_status_counts.push({
-                payment_status_id: rows.payment_status_id,
-                payment_status: rows.payment_status,
-                payment_status_total: statusCount[rows.payment_status_id] || 0,
-                amount_total: amountTotal[rows.payment_status_id] ||0
-            });
+            if (rows.status === 1) {
+                payment_status_counts.push({
+                    payment_status_id: rows.payment_status_id,
+                    payment_status: rows.payment_status,
+                    payment_status_total: statusCount[rows.payment_status_id] || 0,
+                    amount_total: amountTotal[rows.payment_status_id] || 0
+                });
+            }
         });
-        
+
         const data = {
             status: 200,
             message: "Task dashboard Status Count retrieved successfully",
-            task_payment_status_total_list_count:task_payment_status_total_list_count,
-            adha_payment_status_total_amount_count:adha_payment_status_total_amount_count,
-            payment_status_counts:payment_status_counts
+            task_payment_status_total_list_count: task_payment_status_total_list_count,
+            adha_payment_status_total_amount_count: adha_payment_status_total_amount_count,
+            payment_status_counts: payment_status_counts
         };
 
         return res.status(200).json(data);
